931cc96735020aa04a340dbd7ff08aa40ee60bec
Adonis Veneros <adonisiv@iestptrujillo.net>
2024-10-31 10:51:39 -0500

    Inicio

---
+++ scipt.js
@@ -0,0 +1,6 @@
+document.getElementById('sumBtn').addEventListener('click', function() {
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const sum = num1 + num2;
+    document.getElementById('result').textContent = 'Resultado: ' + sum;
+});
